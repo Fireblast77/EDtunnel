0dba2ad9d0f20651150b7a17d6caff49ff302f33
Fireblast77 <ferdipurwandi63@gmail.com>
2024-08-05 06:51:20 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,10 +3,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'a3606cfc-2982-4c0c-a844-025666a2453a';
 
-const พร็อกซีไอพีs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
-
+const พร็อกซีไอพีs = ['129.227.44.166']
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
 // use single พร็อกซีไอพี instead of random
